d36810e962a814aa206edbbeb65c7cc1b3a29ff6
Miracle Kwosah <miraclekwosah17@gmail.com>
2023-09-11 08:19:53 -0700

    2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -7,18 +7,3 @@ def replace_in_list(my_list, idx, element):
     if idx >= 0 and idx < len(my_list):
         my_list[idx] = element
     return (my_list)
-
-================================
-
-3-print_reversed_list_integer.p
-
-#!/usr/bin/python3
-# 3-print_reversed_list_integer.py
-
-
-def print_reversed_list_integer(my_list=[]):
-    """Print all integers of a list in reverse order."""
-    if isinstance(my_list, list):
-        my_list.reverse()
-        for i in my_list:
-            print("{:d}".format(i))
